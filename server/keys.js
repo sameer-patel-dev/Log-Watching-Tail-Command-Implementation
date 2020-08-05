ae394b65a840c87745c788cd406bd4cf7bcc1342
Sameer <sameer.patel@somaiya.edu>
2020-08-05 21:19:09 +0530

    Minor Change

--- server/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://sameer:sameer@cluster0.lde0l.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET:"egedhssjaosjshsidfjvvmvvmvmvkdododddfbfjdkd"
-}
